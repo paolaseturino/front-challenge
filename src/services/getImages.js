99ed8ffd94cf8da877fbc2432fd8ae03875e7091
paolaseturino <paola.seturino.com>
2023-03-08 21:39:49 -0600

    Remove service unused

--- src/services/getImages.js
+++
@@ -1,9 +0,0 @@
-export const getImage =  () => {
-  fetch('https://jsonplaceholder.typicode.com/photos')
-  .then(response => response.json())
-  .then(data => {
-      let r = Math.floor(Math.random() * data.length)
-      return data[r].url
-      }
-    );
-}
